5a87884d4524eea9f3759c020356257ad322059b
Alexandros <77010224+AzizTarek@users.noreply.github.com>
2024-05-01 20:45:41 +0100

    project files

---
+++ Bulky/BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,49 @@
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        
+        private readonly ApplicationDbContext _context;
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _context.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString()) //Example of custom validation
+            {
+                ModelState.AddModelError("Name", "Display Order should not match the Name.");
+            }
+
+            
+			if (obj.Name != null && obj.Name.ToLower() == "test") 
+			{
+				ModelState.AddModelError("", obj.Name+" is an invalid value.");
+			}
+			if (ModelState.IsValid)
+            {
+				_context.Categories.Add(obj);
+				_context.SaveChanges();
+				return RedirectToAction("Index", "Category");
+
+			}
+            return View();
+		}
+
+    }
+}
---
+++ Bulky/BulkyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,34 @@
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base (options) 
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Drama", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "Comedy", DisplayOrder = 3 },
+                new Category { Id = 4, Name = "Thiller", DisplayOrder = 4 }
+                );
+        }
+    }
+}
+
+//Commands for the console
+
+//Adding new table
+//add-migration Add[TableName]TableToDb
+//update-database
+
+//Adding table data
+// add-migration SeedCategoryTable
