3d2195e3f4e80ac2a6f78a68af0f7ddd9c3b44a2
Alexandros <77010224+AzizTarek@users.noreply.github.com>
2024-05-14 17:40:43 +0100

    edit button

--- Bulky/BulkyWeb/Controllers/CategoryController.cs
+++ Bulky/BulkyWeb/Controllers/CategoryController.cs
@@ -7,7 +7,7 @@ namespace BulkyWeb.Controllers
     public class CategoryController : Controller
     {
         
-        private readonly ApplicationDbContext _context;
+        private readonly ApplicationDbContext _context;  //db context
         public CategoryController(ApplicationDbContext context)
         {
             _context = context;
@@ -24,6 +24,42 @@ namespace BulkyWeb.Controllers
         }
         [HttpPost]
         public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString()) //Example of custom validation
+            {
+                ModelState.AddModelError("Name", "Display Order should not match the Name.");
+            }
+
+            
+			if (obj.Name != null && obj.Name.ToLower() == "test") 
+			{
+				ModelState.AddModelError("", obj.Name+" is an invalid value.");
+			}
+			if (ModelState.IsValid)
+            {
+				_context.Categories.Add(obj);
+				_context.SaveChanges();
+				return RedirectToAction("Index", "Category");
+
+			}
+            return View();
+		}
+        public IActionResult Edit(int? categoryId)
+        {
+            if (categoryId==null || categoryId==0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _context.Categories.Find(categoryId);
+
+            if(categoryFromDb==null){
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
         {
             if (obj.Name == obj.DisplayOrder.ToString()) //Example of custom validation
             {
